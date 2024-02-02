3b34dfd50a2ac4d681c5cf050386304f5f73317a
Décio <deciocavalcantedeandrade@gmail.com>
2024-02-02 03:01:00 -0300

    Implementando action em DashboardController

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,10 @@
 module Admin
   class DashboardController < ApiController
     def total_users_number
-      render status: 200, json: 'total_users_number!'
+      total = User.all.size
+      total = User.where(role: params[:by_role]).size if params[:by_role].present?
+
+      render status: 200, json: total
     end
   end
 end
