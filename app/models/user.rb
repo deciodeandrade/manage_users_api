99bf43069d7977be1d676d3b6f4f5f72dbf2458d
Décio <deciocavalcantedeandrade@gmail.com>
2024-02-02 12:12:53 -0300

    Adicionando avatar_image a User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_one_attached :avatar_image
 end
