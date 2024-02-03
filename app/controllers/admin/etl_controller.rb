b134645d8ab65f3e0d8e56b3c36072422b2f02dd
Décio <deciocavalcantedeandrade@gmail.com>
2024-02-03 13:20:59 -0300

    Implementando action make_etl_users

--- app/controllers/admin/etl_controller.rb
+++ app/controllers/admin/etl_controller.rb
@@ -1,11 +1,40 @@
 module Admin
   class EtlController < ApiController
+    require 'csv'
+
     def make_etl_users
-      render status: 200, json: 'make_etl_users!'
+      response = import(params['file'].path)
+
+      render status: 200, json: response[:status]
     end
 
     def etl_users_progress
-      render status: 200, json: 'etl_users_progress!'
+      #count = User.where(file_id: params['file_id']).size
+
+      render status: 200#, json: count
+    end
+
+    private
+
+    def import(path)
+      errors = []
+
+      successful, failed = 0, 0
+
+      CSV.foreach(path, headers: true) do |row|
+        begin
+          user_params = row.to_h
+          User.create!(user_params)
+          successful += 1
+        rescue StandardError => e
+          failed += 1
+          errors.push({user_params: user_params, error: e})
+        end
+      end
+
+      attempts = successful + failed
+
+      {status: {attempts: attempts, successful: successful, failed: failed}, errors: errors}
     end
   end
 end
